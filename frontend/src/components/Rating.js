7aa009eeb4e60bd9d5847b96a23b737923860aed
Ahmad Raza <ahmadrazach26@gmail.com>
2021-12-11 03:50:22 +0500

    Update Rating.js
    Do the coloring of the stars  by code #FDCC0D by default

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -1,26 +1,30 @@
 import React from 'react'
 
-function Rating({value,text}) {
+function Rating({value,text,color}) {
     return (
         <div className='rating'>
            <span>
-               <i className={value>=1?'fas fa-star':value>=0.5?'fas fa-star-half-alt':'far fa-star'}/>
+               <i style={{color}} className={value>=1?'fas fa-star':value>=0.5?'fas fa-star-half-alt':'far fa-star'}/>
            </span>
            <span>
-               <i className={value>=2?'fas fa-star':value>=1.5?'fas fa-star-half-alt':'far fa-star'}/>
+               <i style={{color}} className={value>=2?'fas fa-star':value>=1.5?'fas fa-star-half-alt':'far fa-star'}/>
            </span>
            <span>
-               <i className={value>=3?'fas fa-star':value>=2.5?'fas fa-star-half-alt':'far fa-star'}/>
+               <i style={{color}} className={value>=3?'fas fa-star':value>=2.5?'fas fa-star-half-alt':'far fa-star'}/>
            </span>
            <span>
-               <i className={value>=4?'fas fa-star':value>=3.5?'fas fa-star-half-alt':'far fa-star'}/>
+               <i  style={{color}} className={value>=4?'fas fa-star':value>=3.5?'fas fa-star-half-alt':'far fa-star'}/>
            </span>
            <span>
-               <i className={value>=5?'fas fa-star':value>=4.5?'fas fa-star-half-alt':'far fa-star'}/>
+               <i style={{color}} className={value>=5?'fas fa-star':value>=4.5?'fas fa-star-half-alt':'far fa-star'}/>
            </span>
            <span> {text && text}</span>
         </div>
     )
+
+}
+Rating.defaultProps={
+    color:'#FDCC0D',
 }
 
 export default Rating
