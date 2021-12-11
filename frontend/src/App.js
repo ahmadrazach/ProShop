451499d7a9e4a4fb94623f07cb3d7468a1ffbab4
Ahmad Raza <ahmadrazach26@gmail.com>
2021-12-12 00:42:27 +0500

    Update App.js
    Updated the App.js with the react-router-dom code for the homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,17 +3,27 @@ import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 const App=()=> {
   return (
-    <>
+    <BrowserRouter>
     <Header/>
     <main className="py-3">
       <Container>
-        <HomeScreen/>
+        <Routes>
+         
+        <Route path='/' element={<HomeScreen/>} exact/>
+       
+        </Routes>
+        
       </Container>
       </main>
     <Footer/>
-    </>
+    </BrowserRouter>
   );
 }
 
