62dd0abb5b5ffbc73d5240946077ad01a2d7116b
Ahmad Raza <ahmadrazach26@gmail.com>
2022-03-18 02:40:55 +0500

    Done with Data Seeder Script

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,10 @@ import mongoose from "mongoose";
 
 const connectDB=async()=>{
     try{
-        const conn=await mongoose.connect(process.env.MONGO_URI)
+        const conn=await mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true, useUnifiedTopology: true })
         console.log(`MongoDB Connected: ${conn.connection.host}`)
     }catch(error){
-        console.error(`Error: ${error.message}`)
+        console.error(`Error : ${error.message}`)
         process.exit(1)
     }
 }
