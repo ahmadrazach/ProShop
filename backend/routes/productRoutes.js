0b281ff7c9234452245199670e5c0e9046e2b1b6
Ahmad Raza <ahmadrazach26@gmail.com>
2022-03-22 02:54:17 +0500

    Done with setting up Psotman and checking products and product api

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,6 +6,7 @@ import Product from '../models/productModel.js';
 // @desc Fetch all products
 // @route GET /api/products
 // @access Public
+
 router.get('/',asyncHandler(async(req,res)=>{
     const products=await Product.find({})
     res.json(products)
@@ -14,6 +15,7 @@ router.get('/',asyncHandler(async(req,res)=>{
 // @desc Fetch single product
 // @route GET /api/product/:id
 // @access Public
+
 router.get('/:id',asyncHandler(async(req,res)=>{
     const product= await Product.findById(req.params.id)
     if(product)
