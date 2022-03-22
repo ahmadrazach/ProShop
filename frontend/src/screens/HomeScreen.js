f6102e056f7f40b9750e3b0ca1f210b9a39a69ad
Ahmad Raza <ahmadrazach26@gmail.com>
2022-03-23 02:57:41 +0500

    Done with implementing Loader and Message on homescreen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -2,6 +2,8 @@ import React,{useState,useEffect} from 'react'
 import {Row,Col} from 'react-bootstrap';
 import { useDispatch,useSelector,UseSelector } from 'react-redux';
 import Product from '../components/Product'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
 import { listProducts } from '../actions/productActions';
 //import axios from 'axios'
 
@@ -20,9 +22,9 @@ const HomeScreen=()=> {
           <h1>Latest Products</h1>
           {
             loading?(
-              <h2>Loading...</h2>
+              <Loader/>
             ):error?(
-              <h3>{error}</h3>
+              <Message variant='danger'>{error}</Message>
             ):
               <Row>
             {products.map((product)=>(
