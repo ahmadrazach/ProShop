6bdb5bd0d3d1feea2ad0cd23678dd4752e98d8e4
Ahmad Raza <ahmadrazach26@gmail.com>
2022-03-23 03:18:29 +0500

    Done with making action function of the lproductDetaisl

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,7 +2,8 @@ import {createStore,combineReducers,applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import {composeWithDevTools} from 'redux-devtools-extension'
 import { productListReducer,
-productDetailsReducer } from './reducers/productReducers';
+productDetailsReducer 
+} from './reducers/productReducers';
 const reducer=combineReducers({
     productList:productListReducer,
     productDetails:productDetailsReducer,
