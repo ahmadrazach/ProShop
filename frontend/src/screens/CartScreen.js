d50cf860b8c231b88990f03bb958c6ebb0774330
Ahmad Raza <ahmadrazach26@gmail.com>
2022-03-25 00:47:35 +0500

    Making Shopping to Cart UI - added the cart product showing UI

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -18,6 +18,9 @@ import { addToCart } from '../actions/cartActions'
     const cart=useSelector((state)=>state.cart)
     const{cartItems}=cart
     
+    const removeFromCartHandler=()=>{
+        console.log('remove')
+    }
     useEffect(()=>{
         if(productId){
             dispatch(addToCart(productId,qty))
@@ -33,7 +36,42 @@ import { addToCart } from '../actions/cartActions'
                         Your cart is empty <Link to="/">Go Back</Link>
                     </Message>
                 ):(
-                    <ListGroup variant='flush'></ListGroup>
+                    <ListGroup variant='flush'>
+                        {
+                            cartItems.map(item=>(
+                                <ListGroup.Item key={item.product}>
+                                    <Row>
+                                        <Col md={2}>
+                                            <Image src={item.image} alt={item.name} fluid rounded/>
+                                        </Col>
+                                        <Col md={3}>
+                                            <Link to={`/product/${item.product}`}>{item.name}</Link>
+                                        </Col>
+                                        <Col md={2}>$ {item.price}</Col>
+                                        <Col md={2}>
+                                            <Form.Control as='select' value={item.qty} onChange={(e)=>
+                                                   addToCart(item.product,Number(e.target.value))}>
+                                                        {[...Array(item.countInStock).keys()].map(x=>(
+                                                            <option key={x+1} value={x+1}>
+                                                                {x+1}
+                                                            </option>
+                                                        ))}
+                                            </Form.Control>
+                                        </Col>
+                                        <Col md={2}>
+                                            <Button
+                                            type='button'
+                                            variant='light'
+                                            onClick={()=>removeFromCartHandler(item.product)}
+                                            >
+                                            <i className='fas fa-trash'></i>
+                                            </Button>
+                                        </Col>
+                                    </Row>
+                                </ListGroup.Item>
+                            ))
+                        }
+                    </ListGroup>
                 )}
             </Col>
         </Row>
