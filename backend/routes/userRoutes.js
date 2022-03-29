88357f8f45ef2fb33dc1b67be1512d2bd8a2c9aa
Ahmad Raza <ahmadrazach26@gmail.com>
2022-03-30 02:11:58 +0500

    added middleware for auth

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,10 +4,10 @@ const router =express.Router()
 //import Product from '../models/productModel.js';
 //import { getProducts, getProductsById } from '../controllers/productControllers.js';
 import { authUser, getUserProfile } from "../controllers/userController.js";
-
+import {protect} from '../middleware/authMiddleware.js'
 
 router.post('/login',authUser)
-router.route('/profile').get(getUserProfile)
+router.route('/profile').get(protect,getUserProfile)
 
 
 export default router;
