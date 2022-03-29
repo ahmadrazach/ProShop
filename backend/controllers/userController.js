9556dfc26ffcd8031d4902fbeb60fc12eca5208d
Ahmad Raza <ahmadrazach26@gmail.com>
2022-03-30 02:31:22 +0500

    made the function getUserprofile to get the user data to any routes we want

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -33,20 +33,19 @@ const authUser =asyncHandler(async(req,res)=>{
 
 const getUserProfile =asyncHandler(async(req,res)=>{
 
-    // const user=await User.findById(req.user._id)
-    // if(user && (await user.matchPassword(password))){
-    //     res.json({
-    //         _id:user._id,
-    //         name:user.name,
-    //         email:user.email,
-    //         isAdmin:user.isAdmin,
-    //         token:generateToken(user._id),
-    //     })
-    // }else{
-    //     res.status(401)
-    //     throw new Error('Invalid email or password')
-    // }
-    res.send('Success')
+    const user=await User.findById(req.user._id)
+    if(user ){
+        res.json({
+            _id:user._id,
+            name:user.name,
+            email:user.email,
+            isAdmin:user.isAdmin,
+        })
+    }else{//404 error of user not found
+        res.status(404)
+        throw new Error('User not found')
+    }
+   
 })
 
 
--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,12 +1,39 @@
 import jwt from 'jsonwebtoken'
+import asyncHandler from 'express-async-handler'
 import User from '../models/userModel.js'
 
-const protect= async(req,res,next)=>{
+const protect= asyncHandler(async(req,res,next)=>{
     let token
-    console.log(req.header.authorization)
+    // console.log(req.headers.authorization)
 
-    next()
+    if(req.headers.authorization &&
+        req.headers.authorization.startsWith('Bearer')
+        ){
+            try{
+                //taking the taking from the header
+            token=req.headers.authorization.split(' ')[1]
+            const decoded=jwt.verify(token,process.env.JWT_SECRET)
 
-}
+            // console.log(decoded)
+
+            //fetching user with decoded id
+            
+            req.user=await User.findById(decoded.id).select('-password')
+            next()
+            }
+            //if the token's failed to authenticate
+            catch(error){
+                console.error(error)
+                res.status(401)
+                throw new Error('Not authorized, token failed')
+            }
+        }
+
+    if(!token){
+        res.status(401)
+        throw new Error("Not authorized, no token")
+    }
+
+})
 
 export {protect}
