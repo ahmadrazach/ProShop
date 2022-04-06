bd28c05e337d5abb34f934d4cac6aff361def29a
Ahmad Raza <ahmadrazach26@gmail.com>
2022-04-07 01:42:10 +0500

    making updateUserprofile function in the controller , backend to take the user updated info and save it in the database. made userRouters settings to check if it's update query

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,12 +3,12 @@ import express from "express";
 const router =express.Router()
 //import Product from '../models/productModel.js';
 //import { getProducts, getProductsById } from '../controllers/productControllers.js';
-import { authUser, registerUser,getUserProfile } from "../controllers/userController.js";
+import { authUser, registerUser,getUserProfile, updateUserProfile } from "../controllers/userController.js";
 import {protect} from '../middleware/authMiddleware.js'
 
 router.route('/').post(registerUser)
 router.post('/login',authUser)
-router.route('/profile').get(protect,getUserProfile)
+router.route('/profile').get(protect,getUserProfile).put(protect,updateUserProfile)
 
 
 export default router;
