2e68cd89c9df015658ebb35d4c90c2f56cc767b4
Ahmad Raza <ahmadrazach26@gmail.com>
2022-04-07 00:12:14 +0500

    making a USER_LOGUT action function and dispatching it  in the header when clicking logout

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,15 +2,17 @@ import React from 'react'
 import {useDispatch,useSelector} from 'react-redux'
 import {Navbar,Nav,Container, NavDropdown} from "react-bootstrap";
 import {LinkContainer} from "react-router-bootstrap";
-function Header() {
+import {logout} from '../actions/userActions'
+const Header=()=> {
   
+  const dispatch=useDispatch()
   //taking userstate from global
   const userLogin=useSelector(state=>state.userLogin)
   const {userInfo}=userLogin
 
   //logout handler
   const logoutHandler=()=>{
-    console.log('logout')
+    dispatch(logout())
   }
   return (
         <header>
