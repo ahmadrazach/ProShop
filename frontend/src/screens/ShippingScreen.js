4945d46d3b50d273e1ef00968931e62db5c8021c
Ahmad Raza <ahmadrazach26@gmail.com>
2022-04-16 00:02:41 +0500

    Done with making CheckoutSteps and integrating in the ShippingScreen

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -2,9 +2,9 @@ import React,{useState} from 'react'
 import {Form,Button} from 'react-bootstrap'
 import { useDispatch,useSelector } from 'react-redux'
 import FormContainer from '../components/FormContainer'
+import CheckoutSteps from '../components/CheckoutSteps'
 import {useNavigate} from 'react-router-dom'
 import { saveShippingAddress } from '../actions/cartActions'
-
 const ShippingScreen = () => {
 
     const cart=useSelector(state=>state.cart)
@@ -23,6 +23,7 @@ const ShippingScreen = () => {
     }
   return (
     <FormContainer>
+        <CheckoutSteps step1 step2 />
         <h1>Shipping</h1>
         <Form onSubmit={submitHandler}>
             <Form.Group controlId='address'>
