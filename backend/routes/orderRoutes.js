b7e8921f527edb11f11f6a11940a47b5fe3fc444
Ahmad Raza <ahmadrazach26@gmail.com>
2022-04-20 01:07:00 +0500

    made the backend for finding order by ID and adding route in orderRoutes

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -3,11 +3,11 @@ import express from "express";
 const router =express.Router()
 //import Product from '../models/productModel.js';
 // import { getProducts, getProductById } from '../controllers/productControllers.js';
-import {addOrderItems} from '../controllers/orderController.js'
+import {addOrderItems, getOrderbyId} from '../controllers/orderController.js'
 import {protect} from '../middleware/authMiddleware.js'
 
 router.route('/').post(protect,addOrderItems)
-
+router.route('/:id').get(protect,getOrderbyId)
 
 
 export default router;
