c09d15201f91a06e646d07a31dbf294d74456162
Ahmad Raza <ahmadrazach26@gmail.com>
2022-05-21 16:59:30 +0500

    done with showing orders in the profile section
    add order actions, reducers and constants , store it in the store.js and add it in the ProfileScreen.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -77,7 +77,7 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
 // @route GET /api/orders/myorders
 // @access Private
 const getMyOrders = asyncHandler(async (req, res) => {
-    const order = await Order.find(req.user._id)
+    const orders = await Order.find(req.user._id)
     res.json(orders)
    
   })
