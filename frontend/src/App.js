816fdbaeb1bf311a62f956c44781d78e4f7ea964
Ahmad Raza <ahmadrazach26@gmail.com>
2022-05-22 14:54:10 +0500

    made UI and frontend functionality for editing or creating new product by ProductEditScreen.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,6 +20,7 @@ import OrderScreen from './screens/OrderScreen';
 import UserListScreen from './screens/UserListScreen';
 import UserEditScreen from './screens/UserEditScreen';
 import ProductListScreen from './screens/ProductListScreen';
+import ProductEditScreen from './screens/ProductEditScreen';
 
 const App=()=> {
   return (
@@ -41,6 +42,7 @@ const App=()=> {
         <Route path='/admin/userlist' element={<UserListScreen/>}/>
         <Route path='/admin/user/:id/edit' element={<UserEditScreen/>}/>
         <Route path='/admin/productlist' element={<ProductListScreen/>}/>
+        <Route path='/admin/product/:id/edit' element={<ProductEditScreen/>}/>
         <Route exact path='/' element={<HomeScreen/>} />
         </Routes>
       </Container>
