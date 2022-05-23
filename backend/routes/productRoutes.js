30d0180218c935d528bc8d3dddbc0b0a84ce1f85
Ahmad Raza <ahmadrazach26@gmail.com>
2022-05-23 12:11:41 +0500

    backend for the productReviews

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,13 +2,15 @@ import express from "express";
 //import asyncHandler from "express-async-handler";
 const router =express.Router()
 //import Product from '../models/productModel.js';
-import { getProducts, getProductById, deleteProduct, updateProduct, createProduct } from '../controllers/productControllers.js';
+import { getProducts, getProductById, deleteProduct, updateProduct, createProduct, createProductReview } from '../controllers/productControllers.js';
 import {admin, protect} from '../middleware/authMiddleware.js'
 
 router.route('/')
     .get(getProducts)
     .post(protect,admin,createProduct)
-
+    
+router.route('/:id/reviews')
+    .post(protect,createProductReview)
 
 router.route('/:id')
     .get(getProductById)
